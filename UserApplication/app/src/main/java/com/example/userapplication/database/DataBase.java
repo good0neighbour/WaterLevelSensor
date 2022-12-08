224a98110051d5dabe0b5568653d955f014cd3a8
good0neighbour <notyeas@gmail.com>
2022-12-09 00:37:44 +0900

    Json created
    Json created.

--- UserApplication/app/src/main/java/com/example/userapplication/database/DataBase.java
+++ UserApplication/app/src/main/java/com/example/userapplication/database/DataBase.java
@@ -1,5 +1,6 @@
 package com.example.userapplication.database;
 
+import com.example.userapplication.MainActivity;
 import java.util.Hashtable;
 
 public class DataBase {
@@ -11,10 +12,7 @@ public class DataBase {
                 new UserTable(
                         "amor2022",
                         "00건물 안전관리 시스템",
-                        new DeviceInfo[] {
-                                new DeviceInfo("위치0", "test0"),
-                                new DeviceInfo("위치1", "test1")
-                        }
+                        MainActivity.Instance.LoadJson()
                 )
         );
     }
