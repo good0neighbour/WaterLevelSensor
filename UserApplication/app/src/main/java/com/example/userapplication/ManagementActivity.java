9da0025f3fe4b376f87d7349acfcb3713bb399df
good0neighbour <notyeas@gmail.com>
2022-12-15 21:35:08 +0900

    Descriptions added
    Descriptions for the school report

--- UserApplication/app/src/main/java/com/example/userapplication/ManagementActivity.java
+++ UserApplication/app/src/main/java/com/example/userapplication/ManagementActivity.java
@@ -1,13 +1,11 @@
 package com.example.userapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.userapplication.database.DataBase;
 import java.util.Objects;
 
